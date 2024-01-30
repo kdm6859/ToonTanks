5b45d163064c6099b8eaae544f304ee9cb539769
kdm6859 <kdm6859@naver.com>
2024-01-30 22:02:27 +0900

    C++로 컴포넌트 생성

---
+++ Source/ToonTanks/MyBestFriend.h
@@ -0,0 +1,11 @@
+//#pragma once
+
+#ifndef _MY_BEST_FRIEND_
+#define _MY_BEST_FRIEND_
+
+#define EditAnywhere EditAnywhere
+#define Category Category
+#define VisibleAnywhere VisibleAnywhere
+#define BlueprintCallable BlueprintCallable
+
+#endif
